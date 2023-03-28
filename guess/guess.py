04ee35d471ef8a5b7c5cdb7d0521f5692002e83e
Yujen <maharjanyuzen@gmail.com>
2023-03-28 06:11:57 +0545

    update

--- guess/guess.py
+++ guess/guess.py
@@ -2,20 +2,17 @@ import random
 def main():
     try:
         user=int(input("choose the level from 1-3: "))
-        new_user=level(user)
+        if user==1:
+            number=random.randint(1,10)
+        elif user==2:
+            number=random.randint(1,100)
+        elif user==3:
+            number=random.randint(1,1000)
     except ValueError:
         print("enter integer")
     except UnboundLocalError:
         print("enter number from 1-4")
-def level(user):
-    
-    if user==1:
-        number=random.randint(1,10)
-    elif user==2:
-        number=random.randint(1,100)
-    elif user==3:
-        number=random.randint(1,1000)
-    return number
+
 
 
 if __name__=="__main__":
